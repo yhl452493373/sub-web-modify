99f71394798ab477147c586294a4b4ae9fc4c045
yanghuanglin <yanghuanglin@qq.com>
2024-05-07 18:25:34 +0800

    update

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -475,7 +475,7 @@ export default {
           "sub.cm": "https://sub.cm/short",
         },
         customBackend: {
-          "自用后端": "http://nps.yanghuanglin.cn:25500",
+          "自用后端": "http://sub.yanghuanglin.cn",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
           "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
           "つつ-多地防失联【负载均衡+国内优化】": "https://api.tsutsu.one",
@@ -485,7 +485,7 @@ export default {
           "sub作者&lhie1提供": "https://api.dler.io",
         },
         backendOptions: [
-          {value: "http://nps.yanghuanglin.cn:25500"},
+          {value: "http://sub.yanghuanglin.cn"},
           {value: "https://url.v1.mk"},
           {value: "https://sub.d1.mk"},
           {value: "https://api.tsutsu.one"},
@@ -893,7 +893,7 @@ export default {
         sourceSubUrl: "",
         clientType: "",
         customBackend: this.getUrlParam() == "" ? defaultBackend : this.getUrlParam(),
-        shortType: "https://v1.mk/short",
+        shortType: shortUrlBackend,
         remoteConfig: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_NoAuto.ini",
         excludeRemarks: "",
         includeRemarks: "",
